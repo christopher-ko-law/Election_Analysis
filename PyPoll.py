e22c4afb246a0f9fa2c5210f8f8ccd6c121c5f86
Christopher Law <christopher.ko.law@gmail.com>
2021-05-08 11:13:21 -0400

    pypoll code modifed

--- PyPoll.py
+++ PyPoll.py
@@ -7,35 +7,21 @@
 
 #AT SECTION 3.4.3
 
+# Add our dependencies.
 import csv
 import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Assign a variable to load the file to a path
-file_to_load = os.path.join("Resources","election_results.csv")
-# Assign a variable to the save the file to a path
-file_to_save = os.path.join("analysis","election_analysis.txt")
-
-# Open the election results and read the file
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # To do: perform Analysis
     file_reader = csv.reader(election_data)
-    # Read and print the headers
+
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
-    # Print each row in the CSV file
-    #for row in file_reader:
-    #    print(row[0])
 
-# Close the file
-#election_data.close()
-
-# Create a filename variable to a direct or indirect path to the file
-
-
-with open(file_to_save,"w") as txt_file:
-
-    txt_file.write("Counties in the Election\n--------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
-
-#txt_file.close()
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
